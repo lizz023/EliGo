b0ad6f2e01151e7680680e68461142eabdcf86b8
Eliza <eliiza.023@gmail.com>
2026-06-10 14:15:25 -0500

    Crud completo

--- main.js
+++ main.js
@@ -31,6 +31,19 @@ async function cargarProductos() {
           <td>${producto.nombre}</td>
           <td>$${producto.precio}</td>
           <td>${producto.stock}</td>
+          <td>
+            <button
+              class="btn btn-primary btn-sm"
+              onclick="editarProducto(${producto.id})">
+              Editar
+            </button>
+
+            <button
+              class="btn btn-primary btn-sm"
+              onclick="eliminarProducto(${producto.id})">
+              Eliminar
+            </button>
+          </td>
         </tr>
       `;
 
@@ -72,19 +85,40 @@ formulario.addEventListener(
 
     try {
 
-      await fetch(API_URL, {
+      const id = document.getElementById("productoId").value;
 
-        method: "POST",
+      if (id) {
 
-        headers: {
-          "Content-Type": "application/json"
-        },
+        await fetch(`${API_URL}/${id}`, {
 
-        body: JSON.stringify(producto)
+          method: "PUT",
 
-      });
+          headers: {
+            "Content-Type": "application/json"
+          },
+
+          body: JSON.stringify(producto)
+
+        });
+
+      } else {
+
+        await fetch(API_URL, {
+
+          method: "POST",
+
+          headers: {
+            "Content-Type": "application/json"
+          },
+
+          body: JSON.stringify(producto)
+
+        });
+
+      }
 
       formulario.reset();
+      document.getElementById("productoId").value = "";
 
       cargarProductos();
 
@@ -96,3 +130,58 @@ formulario.addEventListener(
 
   }
 );
+
+async function editarProducto(id) {
+
+  try {
+
+    const respuesta =
+      await fetch(`${API_URL}/${id}`);
+
+    const producto =
+      await respuesta.json();
+
+    document.getElementById("productoId").value =
+      producto.id;
+
+    document.getElementById("nombre").value =
+      producto.nombre;
+
+    document.getElementById("precio").value =
+      producto.precio;
+
+    document.getElementById("stock").value =
+      producto.stock;
+
+  } catch(error) {
+
+    console.error(error);
+
+  }
+
+}
+
+async function eliminarProducto(id) {
+
+  const confirmar =
+    confirm("¿Deseas eliminar este producto?");
+
+  if (!confirmar) return;
+
+  try {
+
+    await fetch(`${API_URL}/${id}`, {
+
+      method: "DELETE"
+
+    });
+
+    cargarProductos();
+
+  } catch(error) {
+
+    console.error(error);
+
+  }
+
+}
